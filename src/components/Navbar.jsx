d762954b448e4f0ad16874fa5ae587e6943d047f
Zwe Thuta <hermez27zz@gmail.com>
2025-08-14 14:58:31 +0630

    tele updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,43 @@
-import { FaGithub, FaFacebook } from "react-icons/fa";
+import { FaGithub, FaFacebook, FaTelegram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <>
       <nav className="w-full h-12 mb-10 flex items-center justify-between py-3">
-        <div className="flex flex-shrink-0 items-center">
-          PORTFOLIO
-        </div>
+        <div className="flex flex-shrink-0 items-center">PORTFOLIO</div>
         <div className="m-8 flex items-center justify-center gap-5 cursor-pointer z-0">
           {/* LinkedIn Icon (optional) */}
           {/* <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin />
           </a> */}
-          
-          <a href="https://github.com/ZweThuta" target="_blank" rel="noopener noreferrer">
+
+          <a
+            href="https://github.com/ZweThuta"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaGithub className="text-2xl" /> {/* Increased icon size */}
           </a>
-          
-          <a href="https://www.facebook.com/share/15QmB6QSQj/" target="_blank" rel="noopener noreferrer">
+
+          <a
+            href="https://www.facebook.com/share/15QmB6QSQj/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaFacebook className="text-2xl" /> {/* Increased icon size */}
+          </a>
+
+          <a
+            href="https://t.me/Hermez27"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTelegram className="text-2xl" /> {/* Increased icon size */}
           </a>
         </div>
       </nav>
     </>
   );
-}
+};
 
 export default Navbar;
